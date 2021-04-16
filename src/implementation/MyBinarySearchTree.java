01b6605b63c3e95ca32f0bd3e7e06236ec3b6a3c
Vaibhav Diwan <divaibhav@gmail.com>
2021-04-16 13:55:47 +0530

    implemented deletion method

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -18,7 +18,7 @@ public class MyBinarySearchTree<E extends Comparable<E>> {
             Node<E> parent = null;
             while (temp != null) {
                 parent = temp;
-                if (element.compareTo(temp.getData()) <= 0) {
+                if (comapre(element, temp) <= 0) {
                     //left subtree
                     temp = temp.getLeft();
                 } else {
@@ -27,7 +27,7 @@ public class MyBinarySearchTree<E extends Comparable<E>> {
             }
             //check whether new node will be left child or right child of
             //parent node
-            if (element.compareTo(parent.getData()) <= 0) {
+            if (comapre(element, parent) <= 0) {
                 //left child
                 parent.setLeft(node);
             } else {
@@ -49,11 +49,11 @@ public class MyBinarySearchTree<E extends Comparable<E>> {
         boolean response = false;
         Node<E> temp = root;
         while (temp != null){
-            if(searchElement.compareTo(temp.getData()) == 0){
+            if(comapre(searchElement, temp) == 0){
                 response = true;
                 break;
             }
-            else if(searchElement.compareTo(temp.getData()) < 0){
+            else if(comapre(searchElement, temp) < 0){
                 temp = temp.getLeft();
             }
             else {
@@ -73,5 +73,113 @@ public class MyBinarySearchTree<E extends Comparable<E>> {
             //step 3
             preOrder(node.getRight());
         }
+    }
+    //delete
+    public void delete(E deletingElement) {
+        Node<E> temp = root;
+        Node<E> parent = null;
+        while (temp != null) {
+            if (comapre(deletingElement, temp) == 0) {
+                break;
+            } else {
+                parent = temp;
+                if (comapre(deletingElement, temp) < 0) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+        }
+        if (temp != null) {
+            //leaf
+            if (isLeaf(temp)) {
+                //root
+                if (parent == null) {
+                    root = null;
+                } else {
+                    if (comapre(deletingElement, parent) < 0) {
+                        parent.setLeft(null);
+                    } else {
+                        parent.setRight(null);
+                    }
+                }
+            }
+
+            //single left child
+            else if (hasLeftChild(temp)) {
+                //root
+                if (parent == null) {
+                    root = root.getLeft();
+                } else {
+                    if (comapre(deletingElement, parent) < 0) {
+                        parent.setLeft(temp.getLeft());
+                    } else {
+                        parent.setRight(temp.getLeft());
+                    }
+                }
+            }
+
+            //single right child
+            else if (hasRightChild(temp)) {
+                //root
+                if (parent == null) {
+                    root = root.getRight();
+                } else {
+                    if (comapre(deletingElement, parent) < 0) {
+                        parent.setLeft(temp.getRight());
+                    } else {
+                        parent.setRight(temp.getRight());
+                    }
+                }
+            }
+
+            //two children
+            else {
+                //get successor
+                Node<E> successor = getSuccessor(temp);
+                //delete successor
+                delete(successor.getData());
+                successor.setLeft(temp.getLeft());
+                successor.setRight(temp.getRight());
+                //root
+                if (parent == null) {
+                    root = successor;
+                } else {
+                    if (comapre(deletingElement, parent) < 0) {
+                        parent.setLeft(successor);
+                    } else {
+                        parent.setRight(successor);
+                    }
+                }
+
+            }
+            //root
+        } else {
+            System.out.println("cannot delete, element not present in binary search tree");
+        }
+    }
+
+    private Node<E> getSuccessor(Node<E> node) {
+        Node<E> temp = node.getRight();
+        while (temp.getLeft() != null) {
+            temp = temp.getLeft();
+        }
+        return temp;
+    }
+
+    private boolean hasRightChild(Node<E> temp) {
+        return temp.getRight() != null && temp.getLeft() == null;
+    }
+
+    private boolean hasLeftChild(Node<E> temp) {
+        return temp.getLeft() != null && temp.getRight() == null;
+    }
+
+    private boolean isLeaf(Node<E> temp) {
+        return temp.getLeft() == null && temp.getRight() == null;
+    }
+
+    private int comapre(E deletingElement, Node<E> temp) {
+        return deletingElement.compareTo(temp.getData());
     }
 }
